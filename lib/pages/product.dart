3f850183c65bb68e8ef9a67d12d92395d1c0bbb0
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-01-12 09:30:23 +0200

    the products page got leaner

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import '../widgets/ui_elements/title_default.dart';
+
 class ProductPage extends StatelessWidget {
   final String title;
   final String description;
@@ -51,14 +53,7 @@ class ProductPage extends StatelessWidget {
             Image.asset(imageURL),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontFamily: 'Oswald',
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              child: TitleDefault(title),
             ),
             Container(
               child: Text(
